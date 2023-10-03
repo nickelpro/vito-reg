f8c7119fbafee8091dfdc9c40b6d22bcd6e12db9
Vito Gamberini <vito@gamberini.email>
2023-10-03 15:16:36 -0400

    chore(modlib): update

--- ports/modlib/portfile.cmake
+++ ports/modlib/portfile.cmake
@@ -1,16 +1,13 @@
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO nickelpro/modlib-test
-  REF 63447b454a69c357da599d272fc45a3bc264535b
-  SHA512 b2bc7b12d6981dbcf4645d92f618c2fbeed5fa52b03d50d04aa28aaf5474b7e3218f0b4d3a9225fe3b831a7a8c22cc8c12a4a74038356dc4c7d3181e3a7727a2
+  REF 9543d55dd759c7be5ef402ce25697457a2dba785
+  SHA512 0
   HEAD_REF main
 )
 
 vcpkg_cmake_configure(
   SOURCE_PATH ${SOURCE_PATH}
-  OPTIONS
-  -DCMAKE_C_COMPILER:STRING=clang
-  -DCMAKE_CXX_COMPILER:STRING=clang++
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
